81e9b374a0e07dcbba3dc5bdff941b64af3d5911
norton16 <bnorton184@gmail.com>
2021-04-20 17:05:13 -0400

    Initial setup

---
+++ src/Components/Events/Events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Events.css'
+
+const Events = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Events
---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Home } from './Home/Home';
+export { default as Contact } from './Contact/Contact';
+export { default as Events } from './Events/Events';

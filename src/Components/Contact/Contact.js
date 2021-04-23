799ff5394c3797ea9444160a80505daec32e04b3
norton16 <bnorton184@gmail.com>
2021-04-23 02:47:22 -0400

    updated contact page styles

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,31 +5,34 @@ import './Contact.css'
 
 const Contact = () => {
     return (
-        <div className="contact-container">
-            <Grid container spacing={0}>
-                <Grid item className="contact-left" xs={12} s={6} m={6} lg={6}>
-                    <div className="contact-blurb">
-                        <h2>Feel free to contact us</h2>
-                        <p>We gladly welcome comments, questions, concerns, or just wanting to say hi!</p>
-                    </div>
+        <div>
+            <Container maxWidth="lg">
+                <Grid container justify="center">
+                    <Grid item className="contact-left" xs={12} s={12} m={6} lg={6}>
+                        <div className="contact-blurb">
+                            <div>
+                                <h1>Feel free to contact us.</h1>
+                                <p>We gladly welcome comments, questions, concerns, or if you just want to say hi!</p>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item justify="center" className="contact-right" xs={12} s={12} m={6} lg={6}>
+                        <div class="contact-form">
+                            <form action="mailto:jdcardsfan99@yahoo.com" method="POST" enctype="multipart/form-data" name="ContactForm">
+                                <label>
+                                    <input type="text" name="name" placeholder="Name" />
+                                    <br />
+                                    <input type="email" name="email" placeholder="Email" />
+                                    <br />
+                                    <input type="text" name="message" placeholder="Message" />
+                                    <br />
+                                </label>
+                                <input type="submit" value="SUBMIT" />
+                            </form>
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item justify="center" className="contact-right" xs={12} s={6} m={6} lg={6}>
-                    <div class="contact-form">
-                        <form action="mailto:jdcardsfan99@yahoo.com" method="POST" enctype="multipart/form-data" name="ContactForm">
-                            <label>
-                                <input type="text" name="name" placeholder="Name..." />
-                                <br />
-                                <input type="email" name="email" placeholder="Email..." />
-                                <br />
-                                <input type="text" name="message" placeholder="Message..." />
-                                <br />
-                            </label>
-                            <input type="submit" value="Submit" />
-                            <label class="warning">*Opens mail application</label>
-                        </form>
-                    </div>
-                </Grid>
-            </Grid>
+            </Container>
         </div>
     )
 }
